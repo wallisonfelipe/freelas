bfd004ec6cedfb94ee8b38cbb7bf0781b8878f9b
wallisonfelipe <wallison_felipe99@hotmail.com>
2022-09-29 22:13:49 -0300

    corrigindo ping pong

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let replaceAllInserter = require('string.prototype.replaceall');
 
+var exec = require('child_process').execSync;
+
 replaceAllInserter.shim();
 
 const express = require('express')
@@ -11,10 +13,10 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Escutando na porta ${port}`)
 })
 
-const WebSocket = require('ws')
+const WebSocket = require('ws');
 var ws = new WebSocket(' wss://ws.99freelas.com.br:3000/socket.io/?idPessoa=261357&userSessionId=A05D4F8AA396448786BDDEDD2184EA47&EIO=4&transport=websocket');
 const { TelegramClient } = require('messaging-api-telegram');
 
@@ -23,27 +25,46 @@ const client = new TelegramClient({
     accessToken: '5740317347:AAGd71cPUYgXFilFO1xivDLv5AqVMisCsCk',
 });
 
-
 let isOpen = false
-ws.onopen = function () {
-    console.log('Conectado!');
-    isOpen = true
-    ws.send(`40`);
-    ws.send(`42["join","profile-261357"]`);
-    ws.send(`42["join","all"]`);
-};
 
-ws.onmessage = function (evt) {
-    console.log(evt.data)
-    if (evt.data.includes("new-message")) {
-        let temp = JSON.parse(evt.data.substring(18, evt.data.length - 2).replaceAll(/\\/g, ''))
-        if (temp.senderId != "261357") {
-            console.log("mensagem enviada pelo telegram")
-            client.sendMessage("1068815207", "Nova mensagem!");
+registerWebsocket();
+
+function registerWebsocket() {
+    ws = new WebSocket(' wss://ws.99freelas.com.br:3000/socket.io/?idPessoa=261357&userSessionId=A05D4F8AA396448786BDDEDD2184EA47&EIO=4&transport=websocket');
+
+    ws.onopen = function () {
+        console.log('Conectado!');
+        isOpen = true
+        ws.send(`40`);
+        ws.send(`42["join","profile-261357"]`);
+        ws.send(`42["join","all"]`);
+    };
+
+    ws.onclose = function () {
+        // websocket is closed.
+        console.log("Connection closed...");
+        isOpen = false
+        registerWebsocket();
+    };
+
+    ws.onmessage = function (evt) {
+        console.log(evt.data)
+        if (evt.data == "2") {
+            // Ping pong event
+            console.log("3")
+            ws.send("3")
         }
-    }
-};
+        if (evt.data.includes("new-message")) {
+            let temp = JSON.parse(evt.data.substring(18, evt.data.length - 2).replaceAll(/\\/g, ''))
+            if (temp.senderId != "261357") {
+                console.log("mensagem enviada pelo telegram")
+                client.sendMessage("1068815207", "Nova mensagem!");
+            }
+        }
+    };
 
-setInterval(function () {
-    ws.send(`3`);
-}, 30 * 1000)
+}
+
+// setInterval(function () {
+//     ws.send(`3`);
+// }, 30 * 1000)
